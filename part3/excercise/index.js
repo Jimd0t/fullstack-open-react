c5c7ec0c45bf1780511fbd10da6b6603b2504b50
Jimd0t <jimmd0tdev@gmail.com>
2025-06-15 20:09:49 +0200

    Excercise 3.2

--- part3/excercise/index.js
+++ part3/excercise/index.js
@@ -30,6 +30,14 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const peopleCount = persons.length;
+  const date = new Date().toString();
+  let responseBody = `<p>Phonebook has info for ${peopleCount} people</p><p>${date}</p>`;
+
+  response.send(responseBody);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
